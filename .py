f799d9697ed00e50a57eacece79ae0d1fb3359f9
Stanley Okonkwo <92925294+motionstudi@users.noreply.github.com>
2022-08-31 01:24:58 -0700

    Create .py

---
+++ .py
@@ -0,0 +1,29 @@
+import math
+ 
+ 
+def fact(num):
+        if num == 1:
+            return 1
+        else:
+            return num * fact(num-1)
+            
+def square(num):
+        return num ** 0.5
+    
+def lognum(num):
+        return math.log(num)
+
+while True:
+        usernum = input("What would you like to caculate ?\n 1. Square root \n 2. Log \n 3. Factorial \n >>>>>>> ")
+ 
+        if usernum == '1':
+            num = (int (input("enter number to sqrt ")))
+            print(square(num))
+        if usernum == '2':
+            num = (int (input("enter number to log ")))
+            print(lognum(num))
+        if usernum == '3':
+            num = (int (input("enter number to factorise ")))
+            print(fact(num))
+        if usernum == "---":
+            break
